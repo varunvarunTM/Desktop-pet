e10318a35cd0527e4426d28feda16c03d32e97d5
varunvarunTM <128722190+varunvarunTM@users.noreply.github.com>
2025-07-27 14:09:53 +0530

    successful running

--- Desktop_pet.py
+++ Desktop_pet.py
@@ -1,14 +1,20 @@
 import tkinter as tk
 import time
+import random
 
 class pet():
     def __init__(self):
         # create a window
         self.window = tk.Tk()
 
-        img = tk.PhotoImage(file='placeholder.png')
+        # placeholder image
+        self.walking_right = [tk.PhotoImage(file='walking_right.gif', format='gif -index %i' % (i)) for i in range(4)]
+        self.frame_index = 0
+        self.img = self.walking_right[self.frame_index]
 
-        self.window.config( highlightbackground ='black')
+        self.timestamp = time.time()
+
+        self.window.config(highlightbackground='black')
 
         self.window.overrideredirect(True)
 
@@ -16,11 +22,12 @@ class pet():
 
         self.window.wm_attributes('-transparentcolor', 'black')
 
-        self.label = tk.Label(self.window , bd=0 , bg='black')
+        self.label = tk.Label(self.window, bd=0, bg='black')
 
-        self.window.geometry('128x128+0+0')
+        self.x = 0
+        self.window.geometry('64x64+{x}+0'.format(x=str(self.x)))
 
-        self.label.configure( image = img)
+        self.label.configure(image=self.img)
 
         self.label.pack()
 
@@ -28,6 +35,20 @@ class pet():
         self.window.mainloop()
 
     def update(self):
+        self.x += 1
+
+        # advance frame if 50ms have passed
+        if time.time() > self.timestamp + 0.05:
+            self.timestamp = time.time()
+            # advance the frame by one, wrap back to 0 at the end
+            self.frame_index = (self.frame_index + 1) % 4
+            self.img = self.walking_right[self.frame_index]
+
+        # create the window
+        self.window.geometry('64x64+{x}+0'.format(x=str(self.x)))
+        self.label.configure(image=self.img)
+        self.label.pack()
+
         self.window.after(10, self.update)
 
 pet()
